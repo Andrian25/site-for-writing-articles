bec58188d334fa92b438a747e8f85c04b175f6cd
Andrian25 <95382174+Andrian25@users.noreply.github.com>
2022-07-06 19:40:17 +0300

    Delete final-project directory

--- final-project/src/main/java/by/teachmeskills/final_project/repository/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package by.teachmeskills.final_project.repository;
-
-import by.teachmeskills.final_project.model.Post;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PostRepository extends CrudRepository<Post, Long> {}
